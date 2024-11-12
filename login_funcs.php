7ba7da77d7dc4c0c243eeaf9b6b2578fca4c494f
lehoangducanh <leanson79@gmail.com>
2024-11-12 09:49:57 +0700

    Add files via upload

---
+++ login_funcs.php
@@ -0,0 +1,42 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    // Khai báo biến $errors để kiểm tra lỗi
+    $errors = [];   
+
+    // Kiểm tra email
+    if (empty(trim($_POST['email']))) {
+        $errors['email']['required'] = 'Email không được để trống';
+    } else {
+        if (!filter_var(trim($_POST['email']), FILTER_VALIDATE_EMAIL)) {
+            $errors['email']['invalid'] = 'Email không hợp lệ';
+        }
+    }
+    
+    // Kiểm tra mật khẩu
+    if (empty(trim($_POST['password']))) {
+        $errors['password']['required'] = 'Mật khẩu không được để trống';
+    } else {
+        // Kiểm tra độ dài mật khẩu
+        if (strlen(trim($_POST['password'])) < 6) {
+            $errors['password']['min_length'] = 'Mật khẩu phải có ít nhất 6 kí tự';
+        }
+        // Kiểm tra xem mật khẩu có chứa ít nhất 1 kí tự thường, 1 kí tự đặc biệt và > 5 chữ số không
+        if (!preg_match('/^(?=.*[a-z])(?=.*[!@#$%^&*])(?=.*[0-9]{5,}).*$/', $_POST['password'])) {
+            $errors['password']['complexity'] = 'Mật khẩu phải chứa ít nhất 1 kí tự thường, 1 kí tự đặc biệt và có ít nhất 5 chữ số';
+        }
+    }
+
+    
+
+    // Nếu không có lỗi thì chuyển hướng đến trang đăng nhập
+    if (empty($errors)) {
+        header('Location: index.php');
+        
+        exit; // Đảm bảo không có mã HTML hoặc mã PHP khác được thực thi sau chuyển hướng
+    }
+
+    echo '<pre>';
+    print_r($errors);
+    echo '<pre>';
+}
+?>
